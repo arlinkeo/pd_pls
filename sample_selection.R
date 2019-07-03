a5b83543a82227cf0dd1a24a9e37df50ecc1ad83
arlinkeo <arlinkeo@gmail.com>
2019-07-03 11:58:10 +0200

    match samples based on MNI coordinates

--- sample_selection.R
+++ sample_selection.R
@@ -16,16 +16,25 @@ sampleInfo <- lapply(donorNames, function(d){
 
 # Sample selection from Arnatkeviciute et al. 2019
 coord <- read.delim("sample_coordinates_roi.txt", col.names = c("sample", "x", "y", "z"))
+parcels <- read.table("parcel_info.txt", col.names = c("ROI", "Name"))
 
 # Match samples by MNI coordinates
-samples <- sapply(sampleInfo, function(s1){
-  s1 <- s1[, c("mni_x", "mni_y", "mni_z")]
+sampleInfo <- lapply(donorNames, function(d){
+  x <- sampleInfo[[d]]
+  s1 <- x[, c("mni_x", "mni_y", "mni_z")]
   s1 <- paste(as.data.frame(t(s1)))
   s2 <- coord[,-1]
   s2 <- paste(as.data.frame(t(s2)))
-  
-  rows <- which(s2 %in% s1)
-  m
+  rows <- match(s1, s2)
+  cbind(fs_roi = coord[rows,1], x)
 })
+saveRDS(sampleInfo, file = "resources/sampleInfo.rds")
 
-
+t <- sapply(sampleInfo2, function(x) {
+  t <- x[!is.na(x$fs_roi), "fs_roi"]
+  sapply(setNames(c(1:34), c(1:34)), function(r) sum(t==r), USE.NAMES = TRUE)
+})
+colnames(t) <- gsub("donor", "Donor ", colnames(t))
+t <- cbind(parcels[rownames(t),], t, Total = apply(t, 1, sum))
+t$Name <- gsub("ctx-lh-", "", t$Name)
+write.table(t, file = "number_of_samples.txt", sep = "\t", row.names = FALSE, quote = FALSE)
