3ce484709268747902b1598ffc953d850209aa8c
arlinkeo <arlinkeo@gmail.com>
2019-09-19 16:22:31 +0200

    heatmap top genes

--- pls.R
+++ pls.R
@@ -1,52 +1,50 @@
 # Partial Least Squares
-
 library(mixOmics)
-library(ggplot2)
 library(ggrepel)
 
 # Parcel info
-parcel_lh <- parcels[grep("lh-", parcels$Name), ]
-parcel_lh$Name <- gsub("ctx-", "", parcel_lh$Name)
-parcel_lh$Name <- gsub("-", "_", parcel_lh$Name)
+parcel_lh <- parcel_id[grep("lh_", parcel_id$name), ]
 
 # X: gene expression in cortical regions
 roi_expr <- lapply(donorNames, function(d){
   e <- brainExpr[[d]]
-  s <- samples_roi[[d]]
-  e <- sapply(parcel_lh$ROI, function(r){ # Mean expression within ROI
+  s <- sample_info[[d]]
+  e <- sapply(parcel_lh$id, function(r){ # Mean expression within ROI
     cols <- which(s$fs_roi == r)
     if (length(cols) > 1) {
-      e <- e[, cols]
-      apply(e, 1, mean)
+      apply(e[, cols], 1, mean)
     } else if (length(cols) == 1){
       e[, cols]
     } else {
       rep(NA, nrow(e))
     }
   }) # genes x samples
-  colnames(e) <- parcel_lh$ROI
+  colnames(e) <- parcel_lh$id
   t(e)
 })
 roi_expr <- apply(simplify2array(roi_expr), c(1,2), function(x) mean(x, na.rm = TRUE)) # Mean across donors
   
 # Y: t-stats of comparing cortical thickness between PD and control,
-tstats <- eq2[, c("Group", "t")]
-rows <- match(parcel_lh$Name, tstats$Group)
-tstats <- tstats[rows, ]
-rownames(tstats) <- parcel_lh$ROI
+tstats <- eq2[grep("lh_", eq2$Group), c("Group", "t")]
+rownames(tstats) <- parcel_lh$id
 tstats$t <- as.numeric(tstats$t)
 
 # PLS
-res <- pls(roi_expr, tstats$t, ncomp = 10, mode = "regression")
-# tune <- perf(res, validation = "Mfold", folds = 10)
+pls1 <- pls(roi_expr, tstats$t, ncomp = 1, mode = "regression")
+# tune <- perf(pls1, validation = "loo") # Leave-one-out cross validation
 # plot(tune$Q2.total)
 # abline(h = 0.0975)
 
+# Correlation of gene expression components
+apply(pls1$variates$X, 2, function(x){
+  cor(x, tstats$t)
+})
+
 # Plot
 df <- data.frame(
-  roi = parcel_lh$ROI, 
-  name = gsub("lh_", "", parcel_lh$Name), 
-  pls1 = res$variates$X[,1],
+  roi = parcel_lh$id, 
+  name = gsub("lh_", "", parcel_lh$name), 
+  pls1 = pls1$variates$X[,2],
   tstats = tstats$t)
 p <- ggplot(df, aes(pls1, tstats)) +
   geom_point(color = "blue") + geom_smooth(method = "lm") +
