3bc594a63739075618fdf0d13f020cce9e025a9a
arlinkeo <arlinkeo@gmail.com>
2019-09-19 16:21:42 +0200

    add group means

--- corticalthickness_conditions.R
+++ corticalthickness_conditions.R
@@ -3,31 +3,11 @@
 
 ########## Mean thickness between conditions ##########
 
-# Read and prepare file
-prepare.file <- function(file){
-  df <- read.delim(file)
-  filter_rows <- grep("Leeftijd|Mean|bankssts", df$Group)
-  df <- df[-c(filter_rows, filter_rows+1), ]
-  df[seq(2, nrow(df), by=2), "Group"] <- df[seq(1, nrow(df), by=2), "Group"]
-  df$hemisphere[grep("lh", df$Group)] <- "L"
-  df$hemisphere[grep("rh", df$Group)] <- "R"
-  df
-}
-
-# plot.mean <- function(df){
-#   mean_ct  <- df[, c("X", "Mean", "hemisphere")]
-#   df <- melt(mean_ct)
-#   ggplot(df) + geom_boxplot(aes(x=interaction(X, hemisphere), y=value)) +
-#     theme_classic()
-# }
-
-group_stats1 <- prepare.file('../cortical_thickness_ttest_jeroen/group_statistics_cortical_thickness_large.txt')
-# p1 <- plot.mean(group_stats1)
-# p1
-
-group_stats2 <- prepare.file('../cortical_thickness_ttest_jeroen/group_statistics_cortical_thickness_small.txt')
-p2 <- plot.mean(group_stats2)
-p2
+group_stats <- read.delim('../cortical_thickness_ttest_jeroen/group_statistics_cortical_thickness_large.txt')
+filter_rows <- grep("Leeftijd|Mean", group_stats$Group)
+group_stats <- group_stats[-c(filter_rows, filter_rows+1), ]
+group_stats[seq(2, nrow(group_stats), by=2), "Group"] <- group_stats[seq(1, nrow(group_stats), by=2), "Group"]
+group_stats$Group <- gsub("_thickness", "", group_stats$Group)
 
 ########## Equality tests between conditions (SPSS results) ##########
 
@@ -40,17 +20,19 @@ eq_tests <- eq_tests[-c(filter_rows, filter_rows+1), ]
 eq_tests[seq(2, nrow(eq_tests), by=2), "Group"] <- eq_tests[seq(1, nrow(eq_tests), by=2), "Group"]
 eq_tests$Group <- gsub("_thickness", "", eq_tests$Group)
 
-eq2 <- eq_tests[eq_tests$variance_assumption == "Equal variances not assumed", -c(3,4)]
-eq2$BH <- p.adjust(eq2$`Sig. (2-tailed)`, method = "BH")
-signif_rows <- eq2$BH < 0.05
+ct_test <- eq_tests[eq_tests$variance_assumption == "Equal variances not assumed", -c(3,4)]
+ct_test$mean_control <- group_stats[group_stats$X == "control", "Mean"]
+ct_test$mean_pd <- group_stats[group_stats$X == "PD", "Mean"]
+ct_test$BH <- p.adjust(ct_test$`Sig. (2-tailed)`, method = "BH")
+signif_rows <- ct_test$BH < 0.05
 
-tab <- data.frame(eq2[signif_rows, c("Group", "Mean Difference", "Std. Error Difference", "BH")])
-colnames(tab) <- c("Region", "Mean Difference", "Std. Error Difference", "BH-adjusted P")
+tab <- data.frame(ct_test[signif_rows, c("Group", "Mean Difference", "mean_control", "mean_pd", "BH")])
+colnames(tab) <- c("Region", "Mean Difference", "Mean control", "Mean PD", "BH-adjusted P")
 tab$Region <- gsub("_", " ", tab$Region)
 tab <- tab[order(tab$`BH-adjusted P`),]
-tab[, 2] <- round(as.numeric(tab[, 2]), digits = 3)
-tab[, 3] <- round(as.numeric(tab[,3]), digits = 3)
-tab[, 4] <- format(tab[, 4], scientific = TRUE, digits = 4)
+tab$`Mean Difference` <- as.numeric(tab$`Mean Difference`)
+tab[, c(2:4)] <- round(tab[, c(2:4)], digits = 3)
+tab$`BH-adjusted P` <- format(tab$`BH-adjusted P`, scientific = TRUE, digits = 3)
 tab[, "Region"] <- gsub("lh", "Left hemisphere", tab[, "Region"])
 tab[, "Region"] <- gsub("rh", "Right hemisphere", tab[, "Region"])
 write.table(tab, file = "output/cortical_thickness_conditions.txt", quote = FALSE, row.names = FALSE, sep = "\t")
