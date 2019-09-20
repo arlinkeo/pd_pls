86fc67695be8aed6681686dcaea12ebacfdb62ba
arlinkeo <arlinkeo@gmail.com>
2019-09-20 15:21:39 +0200

    add samples size to plot titles

--- clinical_scores_distribution.R
+++ clinical_scores_distribution.R
@@ -1,18 +1,22 @@
-setwd("C:/Users/dkeo/surfdrive/pd_imaging")
-library(ggplot2)
 library(reshape2)
 
-tab <- read.table("SPSS from excel from Pat IDs with clin and MRI info_for Arlin4_small.txt", 
+clinicalScores <- read.table("../SPSS from excel from Pat IDs with clin and MRI info_for Arlin4_small.txt", 
                   sep = '\t', header = TRUE)
-colnames(tab)
 
-hist(tab$SENSPDSC, breaks = 50)
-hist(tab$MDS_UPDRS_3, breaks = 50)
-hist(tab$MDS_UPDRS_3_converted, breaks = 50)
+# Overlapping patient IDs between data of cortical thickness and clinical measures
+patientIDs <- as.character(intersect(ct$ID, clinicalScores$ID))
+clinicalScores <- clinicalScores[clinicalScores$ID %in% patientIDs, 10:50]
 
-hist(tab$MMSE, breaks = 50)
+# Check availble scores (non-NA's)
+number_cs <- paste0(colnames(clinicalScores), "\n(n=", apply(clinicalScores, 2, function(x) sum(!is.na(x))), ")")
 
-t <- tab[, -c(1:8, 61:64)]
-df <- melt(t, na.rm = TRUE)
-ggplot(df) + geom_histogram(aes(x=value), bins = 20) +
-  facet_grid(variable, scales = "free", space = "free")
+# Histograms of clinical scores
+df <- clinicalScores
+colnames(df) <- number_cs
+df <- melt(df)
+pdf("output/histogram_clinical_scores.pdf", 12, 12)
+ggplot(df) +
+  geom_histogram(aes(x = value), bins = 20, color = "black", fill = "white") +
+  theme_classic() +
+  facet_wrap(~ variable, scales = "free", nrow = 7)
+dev.off()
